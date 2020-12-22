afb0794f887a81316d6dade0f85f74813607ec1a
Yury Barysevich1 <Yury_Barysevich1@epam.com>
2020-12-22 19:24:15 +0300

    feat: add validators to register form

--- frontend/src/app/pages/register-page/register-page.component.ts
+++ frontend/src/app/pages/register-page/register-page.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register-page',
   templateUrl: './register-page.component.html',
-  styleUrls: ['./register-page.component.scss']
+  styleUrls: ['./register-page.component.scss'],
 })
 export class RegisterPageComponent implements OnInit {
+  isEnabledBtn = false;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
+  userNameFormControl = new FormControl(null, [Validators.required]);
+  emailFormControl = new FormControl(null, [
+    Validators.required,
+    Validators.email,
+  ]);
+  passwordFormControl = new FormControl(null, [
+    Validators.required,
+    Validators.minLength(6),
+  ]);
 
+  onSubmit() {}
 }
