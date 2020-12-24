9365c10bccd4e40fcebfce802d7dd6493c284388
Yury Barysevich1 <Yury_Barysevich1@epam.com>
2020-12-24 03:53:56 +0300

    feat: add validators to form

--- frontend/src/app/pages/link-create-page/link-create-page.component.ts
+++ frontend/src/app/pages/link-create-page/link-create-page.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-link-create-page',
   templateUrl: './link-create-page.component.html',
-  styleUrls: ['./link-create-page.component.scss']
+  styleUrls: ['./link-create-page.component.scss'],
 })
 export class LinkCreatePageComponent implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
+  constructor() {}
+
+  reg = `(https?://)?([\\da-z.-]+)\\.([a-w.]{2,6})[/\\w .-]*/?`;
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      originLink: new FormControl(null, [
+        Validators.required,
+        Validators.pattern(this.reg),
+      ]),
+      describe: new FormControl(null, [Validators.required]),
+    });
   }
 
+  onSubmit() {}
 }
