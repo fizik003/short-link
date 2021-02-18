f47b810fa8b935217a37a0c1e9d5db02e1d09851
Yury Barysevich1 <Yury_Barysevich1@epam.com>
2021-02-18 11:46:25 +0300

    refactor rename component

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,9 +1,5 @@
 import { ContentPageComponent } from './pages/content-page/content-page.component';
 import { AuthPageComponent } from './pages/auth-page/auth-page.component';
-import { YourStatsPageComponent } from './pages/content-page/components/your-stats-page/your-stats-page.component';
-import { TagLinksComponent } from './pages/content-page/components/tag-links/tag-links.component';
-import { LinkDetailsPageComponent } from './pages/content-page/components/link-details-page/link-details-page.component';
-import { MyLinksPageComponent } from './pages/content-page/components/my-links-page/my-links-page.component';
 import { MainPageComponent } from './pages/main-page/main-page.component';
 import { AuthLayoutComponent } from './shared/layouts/auth-layout/auth-layout.component';
 import { NgModule } from '@angular/core';
